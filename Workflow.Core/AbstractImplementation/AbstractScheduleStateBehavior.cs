314e756208eb57ac7b0c46fef78c763c06e1c8bc
Lars Shakya Buch-Jepsen <11513991+larsbuch@users.noreply.github.com>
2020-11-02 05:30:24 +0100

    Updated Workflow.Core

---
+++ Workflow.Core/AbstractImplementation/AbstractScheduleStateBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Workflow.Core.AbstractImplementation
+{
+    /// <summary>
+    /// Convenience implementation of IScheduleStateBehavior
+    /// </summary>
+    public abstract class AbstractScheduleStateBehavior : AbstractStateBehavior, IScheduleStateBehavior
+    {
+        public StateBehaviorType BehaviorType
+        {
+            get
+            {
+                return StateBehaviorType.Schedule;
+            }
+        }
+
+        /// <summary>
+        /// SagaStep is waiting for a sheduled timer before progressing. Will go out of memory just after scheduling as the schedule might be in long time
+        /// </summary>
+        /// <param name="sagaStep"></param>
+        /// <returns></returns>
+        public abstract bool BeforeWaitForState(ISagaStep sagaStep);
+    }
+}
---
+++ Workflow.Core/IAutomaticStateBehavior.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Workflow.Core
+{
+    /// <summary>
+    /// Automatic progress the SagaStep throught the SagaStepState from Initiate to Evaluate
+    /// </summary>
+    public interface IAutomaticStateBehavior:IStateBehavior
+    {
+    }
+}
