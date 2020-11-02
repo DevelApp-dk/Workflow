2cba238da9abf76e2e2bff45c419f7b8ac8ff073
Lars Shakya Buch-Jepsen <11513991+larsbuch@users.noreply.github.com>
2020-11-02 06:13:05 +0100

    Minimalistic implementation

---
+++ Workflow.Core/AbstractImplementation/AbstractStateBehavior.cs
@@ -0,0 +1,27 @@
+﻿using Manatee.Json.Schema;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Workflow.Core.AbstractImplementation
+{
+    /// <summary>
+    /// Placement of utility functions for all StateBehaviors
+    /// </summary>
+    public abstract class AbstractStateBehavior
+    {
+        /// <summary>
+        /// SagaStep is gathering data for the SagaStep
+        /// </summary>
+        /// <param name="sagaStep"></param>
+        /// <returns></returns>
+        public abstract bool Initiate(ISagaStep sagaStep);
+
+        /// <summary>
+        /// Evaluates the gathered data and decides on an outcome 
+        /// </summary>
+        /// <param name="sagaStep"></param>
+        /// <returns></returns>
+        public abstract bool Evaluate(ISagaStep sagaStep);
+    }
+}
---
+++ Workflow.Core/ISaga.cs
@@ -0,0 +1,56 @@
+﻿using Manatee.Json.Schema;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Workflow.Core
+{
+    public interface ISaga
+    {
+        /// <summary>
+        /// Returns the Saga version
+        /// </summary>
+        /// <returns></returns>
+        string SagaVersion { get; }
+
+        /// <summary>
+        /// Returns the first SagaStep of the Saga
+        /// </summary>
+        /// <returns></returns>
+        ISagaStep FirstSagaStep { get; }
+
+        /// <summary>
+        /// Returns the latest SagaStep of the Saga
+        /// </summary>
+        /// <returns></returns>
+        ISagaStep LatestSagaStep { get; }
+
+        /// <summary>
+        /// Possibility to check if SagaStep data is valid
+        /// </summary>
+        /// <param name="sagaStep"></param>
+        /// <returns></returns>
+        bool DataIsValid(ISagaStep sagaStep);
+
+        /// <summary>
+        /// Possibility to check if SagaStep DataJsonSchema is valid before progressing
+        /// </summary>
+        /// <param name="sagaStep"></param>
+        /// <returns></returns>
+        bool DataJsonSchemaIsValid(ISagaStep sagaStep);
+
+        /// <summary>
+        /// Possibility to check validation details of SagaStep data
+        /// </summary>
+        /// <param name="sagaStep"></param>
+        /// <returns></returns>
+        SchemaValidationResults CheckData(ISagaStep sagaStep);
+
+        /// <summary>
+        /// Possibility to check validation details of SagaStep DataJsonSchema. Returns null if DataJsonSchema is not found
+        /// </summary>
+        /// <param name="sagaStep"></param>
+        /// <returns></returns>
+        MetaSchemaValidationResults CheckDataJsonSchema(ISagaStep sagaStep);
+    }
+}
