32a1ab47e056cb34f25bb8e747bd48a3cd142832
Lars Shakya Buch-Jepsen <11513991+larsbuch@users.noreply.github.com>
2020-11-11 05:42:05 +0100

    Abstract made correct

---
+++ JsonSchemaBuilder/IJsonSchemaDefinition.cs
@@ -0,0 +1,31 @@
+﻿using Manatee.Json.Schema;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevelApp.JsonSchemaBuilder
+{
+    public interface IJsonSchemaDefinition
+    {
+        /// <summary>
+        /// Returns the name of the schema
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// The description of the schema
+        /// </summary>
+        string Description { get; }
+
+        /// <summary>
+        /// Returns the complete JsonSchema
+        /// </summary>
+        JsonSchema JsonSchema { get; }
+
+        /// <summary>
+        /// Writes the schema to filePath
+        /// </summary>
+        /// <param name="filePath"></param>
+        void WriteSchemaToFile(string filePath);
+    }
+}
